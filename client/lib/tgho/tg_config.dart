b2b6cae4fc8c6d8cc186d008bd7bcd93abb7ae6a
malikkurosaki <kurosakiblackangel@gmail.com>
2022-03-21 14:34:01 +0800

    push Mon Mar 21 14:34:01 WITA 2022

--- client/lib/tgho/tg_config.dart
+++
@@ -1,8 +0,0 @@
-class TgConfig {
-  static const String host = 'localhost';
-  static const int port = 3000;
-  static const String prefix = 'api';
-  static const String protocol = 'http';
-  static const String version = 'v2';
-  static const String baseUrl = '$protocol://$host:$port/$prefix/$version';
-}
