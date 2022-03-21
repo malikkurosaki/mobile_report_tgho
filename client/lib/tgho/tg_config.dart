bbdd9c6e929c20d02116b8a920734b33a1dfba08
malikkurosaki <kurosakiblackangel@gmail.com>
2022-03-21 14:34:58 +0800

    push Mon Mar 21 14:34:58 WITA 2022

---
+++ client/lib/tgho/tg_config.dart
@@ -0,0 +1,10 @@
+
+class TgConfig {
+  static const String host = 'report-mobile-dev.probussystem.net';
+  static const int port = 3000;
+  static const String prefix = 'api';
+  static const String protocol = 'http';
+  static const String version = 'v2';
+  static const String baseUrl = 'http://:3100//';
+}
+
